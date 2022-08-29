6a7b396fae47c13b51c56d14e516c6be1dd4e151
Tobiasroy <tobiasroy94@gmail.com>
2022-08-29 12:38:11 +0200

    Sales changes

--- ERPRO/Sales/Sales.cs
+++ ERPRO/Sales/Sales.cs
@@ -11,6 +11,11 @@ namespace ERPRO.SalesNS
         Confirmed,
         Packed
     }
+    public enum Storage {
+        Local,
+        External
+    }
+
     public class SalesOrder
     {
         public int OrderID { get; set; }
@@ -18,9 +23,23 @@ namespace ERPRO.SalesNS
         public DateTime TimeOfAcceptance { get; set; }
         public int PersonID { get; set; }
         public Status Status { get; set; }
-        public List<string> OrderLines { get; set; }
-        public decimal Total { get; private set; }
+        public List<SalesOrderLine> OrderLines { get; } = new List<SalesOrderLine>();
+        public decimal GetTotalPrice() {
+            decimal totalprice = 0;
+            foreach (var product in OrderLines)
+            {
+                totalprice += product.ProductSellingPrice;
+            }
+            return totalprice;
+        }
     }
-
-
+    public class SalesOrderLine 
+    {
+        public SalesOrder SalesOrder { get; set; }
+        int ProductItemID { get; set; }
+        string ProductName { get; set; }
+        public decimal ProductSellingPrice { get; set; }
+        public Storage Storage { get; set; }
+        public decimal SaleQty { get; set; }
+    }
 }
